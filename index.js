222860ec5c6ddeacb2183bfb0990cb258dfa89eb
que0ta <queota@supermail.com>
2024-10-31 10:36:46 +0200

    test information page

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req,res) => {
     res.render('index.ejs');
 });
 
+app.get('/information', (req,res) => {
+    res.render('information.ejs');
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
